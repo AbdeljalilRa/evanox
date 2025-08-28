a2a23dd579f9b7559c12e9f2c11bc9c9775a146b
Mehdi <khammali26@gmail.com>
2025-08-28 14:53:44 +0100

    add back to top button functionality with smooth scroll behavior

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,25 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+// Back to Top Component
+window.backToTop = () => ({
+    showButton: false,
+    
+    init() {
+        // Show/hide button based on scroll position
+        window.addEventListener('scroll', () => {
+            this.showButton = window.pageYOffset > 300;
+        });
+    },
+    
+    scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+});
+
 window.Alpine = Alpine;
 
 Alpine.start();
