1e9743842d840ba99075626aa90109d4fd389b85
Mehdi <khammali26@gmail.com>
2025-09-01 23:32:33 +0100

    add shopping bag icon with item count display for improved user interaction

--- resources/views/layouts/store/navbar.blade.php
+++ resources/views/layouts/store/navbar.blade.php
@@ -38,12 +38,13 @@
                                 d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                         </svg>
                     </button>
-                    <button class="text-white hover:text-gray-300 transition-colors">
+                    <button class="text-white hover:text-gray-300 transition-colors bag-icon relative" id="bag-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
                             stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
                         </svg>
+                        <span class="bag-count absolute -top-1 -right-1 bg-white text-black text-xs rounded-full h-4 w-4 flex items-center justify-center font-bold hidden">0</span>
                     </button>
                     <!-- Desktop Dropdown Menu Button -->
                     <button class="hidden md:block text-white hover:text-gray-300 transition-colors"
